d2d82509f835fd9325694f23f21adfa0ad05fa6d
luguodong910208 <31837352+luguodong910208@users.noreply.github.com>
2018-05-23 19:49:32 +0800

    add files

---
+++ demo1/src/bird/Bird.java
@@ -0,0 +1,9 @@
+package bird;
+
+public class Bird {
+	public double weight;
+	public void fly() {
+		System.out.println("我在天空自有地飞行...");
+		System.out.println("我是一只小胖鸟！重" + weight + "g!");
+	}
+}
---
+++ demo1/src/bird/Ostrich.java
@@ -0,0 +1,12 @@
+package bird;
+
+public class Ostrich extends Bird {
+	public void fly() {
+		System.out.println("我只能在地上奔跑...");
+	}
+	public static void main(String[] args) {
+		Bird bird = new Bird();
+		bird.weight = 1000;
+		bird.fly();
+	}
+}
