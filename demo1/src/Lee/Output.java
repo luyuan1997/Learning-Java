b22ae18ac96c95ff072405dee3d9bd8119f5d236
Winston Lu <992090718@qq.com>
2018-06-03 19:11:51 +0800

    add

---
+++ demo1/src/Lee/Output.java
@@ -0,0 +1,23 @@
+package Lee;
+
+public interface Output {
+	//接口里定义的成员变量只能是常量
+	int MAX_CACHE_LINE = 50;
+	//接口里定义的普通方法只能是public的抽象方法
+	void out();
+	void getData(String msg);
+	//在接口里定义默认方法，需要使用default修饰
+	default void print(String... msgs) {
+		for (String msg : msgs) {
+			System.out.println(msg);
+		}
+	}
+	//在接口定义默认方法，需要使用default方法
+	default void test() {
+		System.out.println("默认的test()方法");
+	}
+	//在接口定义类的方法，需要使用static修饰
+	static String staticTest() {
+		return "接口里的类方法";
+	}
+}
