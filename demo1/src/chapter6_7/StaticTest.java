d1af4dbe61c65e9ac4610e1395beb7de6996311d
Winston Lu <992090718@qq.com>
2018-06-04 17:28:26 +0800

    add

---
+++ demo1/src/chapter6_7/StaticTest.java
@@ -0,0 +1,12 @@
+package chapter6_7;
+
+public class StaticTest {
+	//定义一个非静态的内部类，是一个空类
+	private class In{};
+	//外部类的静态方法
+	public static void main(String[] args) {
+		//下面代码引发编程异常，因为静态成员（main()方法）
+//		无法访问非静态成员(In类)
+//		new In();
+	}
+}
