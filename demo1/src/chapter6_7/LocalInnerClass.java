ef76940db91fd852d1685135a624003f5bc17c97
Winston Lu <992090718@qq.com>
2018-06-04 22:16:45 +0800

    add

---
+++ demo1/src/chapter6_7/LocalInnerClass.java
@@ -0,0 +1,19 @@
+package chapter6_7;
+
+public class LocalInnerClass {
+	public static void main(String[] args) {
+		//定义局部内部类
+		class InnerBase{
+			int a;
+		}
+		//定义局部内部类的子类
+		class InnerSub extends InnerBase{
+			int b;
+		}
+		//创建局部内部类的对象
+		InnerSub is = new InnerSub();
+		is.a = 5;
+		is.b = 8;
+		System.out.println("InnerSub对象的a和b实例变量是："+is.a+","+is.b);
+	}
+}
