58da363dcb553bce8e7bb9d7810fcb0117787e5f
Winston Lu <992090718@qq.com>
2018-06-04 17:45:24 +0800

    add

---
+++ demo1/src/chapter6_7/StaticInnerClassTest.java
@@ -0,0 +1,17 @@
+package chapter6_7;
+
+public class StaticInnerClassTest {
+	private int prop1 = 5;
+	private static int prop2 = 9;
+	static class StaticInnerClass{
+		//静态内部类可以包含静态成员
+		private static int age;
+		public void acessOuterProp() {
+			//下面代码出现错误
+			//静态内部类无法访问外部类的实例变量
+//			System.out.println(prop1);
+			//下面代码正常
+			System.out.println(prop2);
+		}
+	}
+}
