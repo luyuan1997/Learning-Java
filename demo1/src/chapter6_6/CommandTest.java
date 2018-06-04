f19fa39f2e8110cd549a4c90539528cf0f55b004
Winston Lu <992090718@qq.com>
2018-06-04 12:02:32 +0800

    add

--- demo1/src/chapter6_6/CommandTest.java
+++ demo1/src/chapter6_6/CommandTest.java
@@ -1,5 +1,13 @@
 package chapter6_6;
 
 public class CommandTest {
-
+	public static void mian(String[] args) {
+		ProcessArray pa = new ProcessArray();
+		int[] target = {3,-4,6,4};
+		//第一次处理数组，具体处理行为取决于PrintCommand
+		pa.process(target, new PrintCommand());
+		System.out.println("------------------");
+		//第二次处理数组，具体处理行为取决于AddCommand
+		pa.process(target, new AddCommand());
+	}
 }
