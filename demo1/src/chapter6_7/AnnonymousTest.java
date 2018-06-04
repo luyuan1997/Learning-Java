2f2a8c6d20ef397817e6597a69b00f86a9485019
Winston Lu <992090718@qq.com>
2018-06-04 22:27:27 +0800

    add

---
+++ demo1/src/chapter6_7/AnnonymousTest.java
@@ -0,0 +1,25 @@
+package chapter6_7;
+interface Product{
+	public double getPrice();
+	public String getName();
+}
+public class AnnonymousTest {
+	public void test(Product p) {
+		System.out.println("购买了一个"+p.getName()+",花掉了"+p.getPrice());
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AnnonymousTest ta = new AnnonymousTest();
+		//调用test()方法时，需要传入一个Product参数
+		//此处传入其匿名实现类的实例
+		ta.test(new Product() {
+			public double getPrice() {
+				return 527.8;
+			}
+			public String getName() {
+				return "AGP显卡";
+			}
+		});
+	}
+
+}
