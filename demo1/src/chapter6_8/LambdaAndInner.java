9548cc73da291b432d98ad31bce28d630fe2e237
Winston Lu <992090718@qq.com>
2018-06-05 22:06:47 +0800

    add

---
+++ demo1/src/chapter6_8/LambdaAndInner.java
@@ -0,0 +1,32 @@
+package chapter6_8;
+@FunctionalInterface
+interface Displayable{
+	//定义一个抽象方法和默认方法
+	void display();
+	default int add(int a, int b) {
+		return a+b;
+	}
+}
+public class LambdaAndInner {
+	private int age = 20;
+	private static String name = "疯狂软件教育中心";
+	public void test() {
+		String book = "疯狂java讲义";
+		Displayable dis = ()->{
+			//访问“effectively final”的局部变量
+			System.out.println("book局部变量为："+book);
+			//访问外部类的实例变量和类变量
+			System.out.println("外部类的age实例变量为："+age);
+			System.out.println("外部类的name类变量为："+name);
+		};
+		dis.display();
+		//调用dis对象从接口中继承的add()方法
+		System.out.println(dis.add(3, 5));
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LambdaAndInner lambda = new LambdaAndInner();
+		lambda.test();
+	}
+
+}
