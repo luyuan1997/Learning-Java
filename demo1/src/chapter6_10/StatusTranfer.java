bb74ad034c6c8a2d9033c77bf0d5bb7afc5f54f5
Winston Lu <992090718@qq.com>
2018-06-06 16:42:08 +0800

    add

---
+++ demo1/src/chapter6_10/StatusTranfer.java
@@ -0,0 +1,15 @@
+package chapter6_10;
+
+public class StatusTranfer {
+	public static void test() {
+		String a = new String("轻量级Java EE企业应用实战");
+		//可恢复状态
+		a = new String("疯狂java讲义");
+		//可达状态
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test();
+	}
+
+}
