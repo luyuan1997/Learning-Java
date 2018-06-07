8681a84df2f6be109b13bbcdd6b1386f96f97d29
Winston Lu <992090718@qq.com>
2018-06-07 14:39:08 +0800

    add

---
+++ demo1/src/chapter7_1/ScannerKeyBoardTest.java
@@ -0,0 +1,20 @@
+package chapter7_1;
+
+import java.util.Scanner;
+
+public class ScannerKeyBoardTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.in代表标准输入，就是键盘输入
+		Scanner sc = new Scanner(System.in);
+		//增加下面一行将只能回车作为分隔符
+//		sc.useDelimiter("\n");
+//		判断是否还有下一个输入项
+		while (sc.hasNext()) {
+//			输出输入项
+			System.out.println("键盘输入的内容是："+sc.next());
+		}
+	}
+
+}
