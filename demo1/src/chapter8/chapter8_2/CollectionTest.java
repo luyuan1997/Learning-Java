1fdbf15c9f063d72b401e4dc8dc6b3a9447cb896
Winston Lu <992090718@qq.com>
2018-06-09 18:39:03 +0800

    update

--- demo1/src/chapter8/chapter8_2/CollectionTest.java
+++ demo1/src/chapter8/chapter8_2/CollectionTest.java
@@ -2,6 +2,7 @@ package chapter8.chapter8_2;
 
 import java.util.Collection;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class CollectionTest {
 
@@ -18,7 +19,21 @@ public class CollectionTest {
 		System.out.println("c集合的元素个数为："+c.size());
 		//判断是否包含指定字符串
 		System.out.println("c集合是否包含\"孙悟空\"字符串："+c.contains("孙悟空"));
-		
+		c.add("轻量级Java EE企业应用实战");
+		System.out.println("c集合的元素："+c);
+		Collection books = new HashSet();
+		books.add("轻量级Java EE企业应用实战");
+		books.add("疯狂Java讲义");
+		System.out.println("c集合是否完全包含books集合？"+c.containsAll(books));
+		//用c集合减去books集合里的元素
+		c.removeAll(books);
+		System.out.println("c集合的元素："+c);
+		//删除c集合里的所有元素
+		c.clear();
+		System.out.println("c集合的元素："+c);
+		//控制books集合里只剩下c集合里也包含的元素
+		books.retainAll(c);
+		System.out.println("books集合的元素："+books);
 	}
 
 }
