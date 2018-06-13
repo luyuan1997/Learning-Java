47c04dab46d1027b3b820bbae493433b8ba698fe
Winston Lu <992090718@qq.com>
2018-06-13 13:32:59 +0800

    add

---
+++ demo1/src/chapter8/chapter8_3/TreeSetTest4.java
@@ -0,0 +1,32 @@
+package chapter8.chapter8_3;
+
+import java.util.TreeSet;
+
+class M{
+	int age;
+	public M(int age){
+		this.age =age;
+	}
+	public String toString() {
+		return "M[age:"+age+"]";
+	}
+}
+
+public class TreeSetTest4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeSet ts = new TreeSet((o1, o2) -> {
+			//(M)o1是指将o1对象强制转换为M类型
+			M m1 =(M)o1;
+			M m2 = (M)o2;
+			//根据M对象的age属性来决定大小,age越大，M对象反而越小
+			return m1.age > m2.age ? -1:m1.age < m2.age ? 1:0;
+		});
+		ts.add(new M(5));
+		ts.add(new M(-3));
+		ts.add(new M(9));
+		System.out.println(ts);
+	}
+
+}
