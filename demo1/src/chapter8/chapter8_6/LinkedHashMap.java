3192e09b5e1ea9a83e20363b696b3937dea8137a
Winston Lu <992090718@qq.com>
2018-06-13 23:16:54 +0800

    add

---
+++ demo1/src/chapter8/chapter8_6/LinkedHashMap.java
@@ -0,0 +1,15 @@
+//package chapter8.chapter8_6;
+//
+//
+//public class LinkedHashMap {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		LinkedHashMap scores = new LinkedHashMap();
+//		scores.put("ÓïÎÄ",80);
+//		scores.put("ÊýÑ§",82);
+//		scores.put("Ó¢Óï",89);
+//		scores.forEach((key, value) -> System.out.println(key+"-->"+value));
+//	}
+//
+//}
