f17b6252871589b0c95107d87b54fb53f9fec1c2
Winston Lu <992090718@qq.com>
2018-06-15 11:29:47 +0800

    add

---
+++ demo1/src/chapter8/chapter8_8/SearchTest.java
@@ -0,0 +1,26 @@
+package chapter8.chapter8_8;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SearchTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList nums = new ArrayList();
+		nums.add(2);
+		nums.add(-5);
+		nums.add(3);
+		nums.add(0);
+		System.out.println(nums);
+		System.out.println(Collections.max(nums));
+		System.out.println(Collections.min(nums));
+		Collections.replaceAll(nums, 0, 1);
+		System.out.println(nums);
+		System.out.println(Collections.frequency(nums, -5));
+		Collections.sort(nums);
+		System.out.println(nums);
+		System.out.println(Collections.binarySearch(nums, 3));
+	}
+
+}
