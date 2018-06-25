cd3734f9e9b08c529058f5758359fb8418c23356
Winston Lu <992090718@qq.com>
2018-06-25 10:03:24 +0800

    update

--- demo1/src/chapter9/chapter9_4/InferenceTest.java
+++ demo1/src/chapter9/chapter9_4/InferenceTest.java
@@ -1,12 +1,25 @@
 package chapter9.chapter9_4;
 
-
+class MyUtil<E>{
+	public static <Z> MyUtil<Z> nil(){
+		return null;
+	}
+	public static <Z> MyUtil<Z> cons(Z head, MyUtil<Z> tail){
+		return null;
+	}
+	E head() {
+		return null;
+	}
+}
 
 public class InferenceTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MyUtil<String> ls = MyUtil.nil();
+		MyUtil<String> mu = MyUtil.<String>nil();
+		MyUtil.cons(42, MyUtil.nil());
+		MyUtil.cons(42, MyUtil.<Integer>nil());
 	}
 
 }
