606956f11a7b9648667125db71c958a26114b2ea
Winston Lu <992090718@qq.com>
2018-06-26 23:40:17 +0800

    add

---
+++ demo1/src/chapter10/chapter10_3/Throws.java
@@ -0,0 +1,13 @@
+package chapter10.chapter10_3;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Throws {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		FileInputStream fis = new FileInputStream("a.txt");
+	}
+
+}
