ebe41aabd40a290b439b650c220d475b9866ee78
Winston Lu <992090718@qq.com>
2018-06-27 18:52:28 +0800

    add

---
+++ demo1/src/chapter10/chapter10_4/Throw2.java
@@ -0,0 +1,16 @@
+//package chapter10.chapter10_4;
+//
+//public class Throw2 {
+//
+//	public static void main(String[] args) throws Exception {
+//		// TODO Auto-generated method stub
+//		try {
+//			new FileOutStream("a.txt");
+//		}
+//		catch{
+//			ex.printStackTrace();
+//			throw ex;
+//		}
+//	}
+//
+//}
