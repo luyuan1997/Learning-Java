2ed609dfe85c178cd4d19a38f8811ec74b9c1e8f
Winston Lu <992090718@qq.com>
2018-06-27 11:42:18 +0800

    add

---
+++ demo1/src/chapter10/chapter10_4/Throw.java
@@ -0,0 +1,33 @@
+package chapter10.chapter10_4;
+
+public class Throw {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			//调用声明抛出Checked异常的方法，要么显式捕获该异常
+			//要么在main方法中再次声明抛出
+			throwChecked(-3);
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		//调用声明抛出Runtime异常的方法既可以显式捕获该异常
+		//也可以不理会该异常
+		throwRuntime(3);
+	}
+	public static void throwChecked(int a) throws Exception{
+		if (a>0) {
+			//自行抛出Exception异常
+			//该代码必须处于try块里，或处于带throws声明的方法中
+			throw new Exception("a的值大于0，不符合要求");
+		}
+	}
+	public static void throwRuntime(int a) {
+		if(a>0) {
+			//自行抛出RuntimeException异常,既可以显式捕获该异常
+			//也可以完全不理会该异常，把该异常交给该方法调用者处理
+			throw new RuntimeException("a的值大于0，不符合要求");
+		}
+	}
+}
