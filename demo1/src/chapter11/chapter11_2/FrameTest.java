fec751d84789abfe3be5ce9d6f73ca158e8ae20c
Winston Lu <992090718@qq.com>
2018-06-28 08:41:04 +0800

    add

---
+++ demo1/src/chapter11/chapter11_2/FrameTest.java
@@ -0,0 +1,14 @@
+package chapter11.chapter11_2;
+
+import java.awt.Frame;
+
+public class FrameTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Frame f = new Frame("²âÊÔ´°¿Ú");
+		f.setBounds(30,30,250,200);
+		f.setVisible(true);
+	}
+
+}
