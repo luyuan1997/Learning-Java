77576c20ca177a8bb842e55f6f7e15847f13e2df
Winston Lu <992090718@qq.com>
2018-06-28 08:04:36 +0800

    add

--- demo1/src/chapter10/chapter10_4/Throw2.java
+++ demo1/src/chapter10/chapter10_4/Throw2.java
@@ -1,16 +1,19 @@
-//package chapter10.chapter10_4;
-//
-//public class Throw2 {
-//
-//	public static void main(String[] args) throws Exception {
-//		// TODO Auto-generated method stub
-//		try {
-//			new FileOutStream("a.txt");
-//		}
-//		catch{
-//			ex.printStackTrace();
-//			throw ex;
-//		}
-//	}
-//
-//}
+package chapter10.chapter10_4;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class Throw2 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		try {
+			new FileOutputStream("a.txt");
+		}
+		catch(Exception ex){
+			ex.printStackTrace();
+			throw ex;
+		}
+	}
+
+}
