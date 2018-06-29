a69406148c3e5bb70864077e6e24c1e3d24733d8
Winston Lu <992090718@qq.com>
2018-06-29 23:14:02 +0800

    add

--- demo1/src/chapter11/chapter11_7/SimpleDraw.java
+++ demo1/src/chapter11/chapter11_7/SimpleDraw.java
@@ -1,7 +1,16 @@
 package chapter11.chapter11_7;
 
+import java.awt.Button;
+import java.awt.Frame;
+
 public class SimpleDraw {
 
+	private final String RECT_SHAPE = "rect";
+	private final String OVAL_SHAPE = "oval";
+	private Frame f = new Frame("简单绘图");
+	private Button rect = new Button("绘制矩形");
+	private Button oval = new Button("绘制圆形");
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
