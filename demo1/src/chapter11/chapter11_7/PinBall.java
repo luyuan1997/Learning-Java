fc6c4c14678302511962e562622c5e0caceeefcd
Winston Lu <992090718@qq.com>
2018-06-30 08:07:36 +0800

    add

---
+++ demo1/src/chapter11/chapter11_7/PinBall.java
@@ -0,0 +1,26 @@
+package chapter11.chapter11_7;
+
+import java.awt.Frame;
+import java.util.Random;
+
+public class PinBall {
+	
+	//桌面的高度和款速
+	private final int TABLE_WIDTH = 300;
+	private final int TABLE_HEIGHT = 400;
+	//球拍的垂直位置
+	private final int RACKET_Y = 340;
+	//球拍的高度和宽度
+	private final int RACKET_HEIGHT = 20;
+	private final int RECKET_WIDTH = 60;
+	//小球的大小
+	private final int BALL_SIZE = 16;
+	private Frame f =new Frame("弹球游戏");
+	Random rand = new Random();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
