a9c0a4459ced2ff8b4a715ee3d87cfa57bfa5a77
Winston Lu <992090718@qq.com>
2018-06-30 22:18:29 +0800

    add

--- demo1/src/chapter11/chapter11_8/ZoomImage.java
+++ demo1/src/chapter11/chapter11_8/ZoomImage.java
@@ -1,10 +1,27 @@
 package chapter11.chapter11_8;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
 public class ZoomImage {
+	
+	private final int WIDTH = 80;
+	private final int HEIGHT = 60;
+	BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	Graphics g = image.getGraphics();
+	public void zoom() throws Exception{
+		Image srcImage = ImageIO.read(new File("image/board.jpg"));
+		g.drawImage(srcImage, 0, 0, WIDTH, HEIGHT,null);
+		ImageIO.write(image, "jpeg", new File(System.currentTimeMillis() + ".jpg"));
+	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-
+		new ZoomImage().zoom();
 	}
 
 }
