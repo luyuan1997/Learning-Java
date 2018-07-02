c7f9c25fa33ec3f8683cdf4d75a26441fa8e030d
Winston Lu <992090718@qq.com>
2018-07-02 23:17:22 +0800

    add

---
+++ demo1/src/chapter12/chapter12_2/ImageViewer.java
@@ -0,0 +1,10 @@
+package chapter12.chapter12_2;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+
+public class ImageViewer {
+	final int PREVIEW_SIZE = 100;
+	JFrame jf = new JFrame("¼òµ¥Í¼Æ¬²é¿´Æ÷");
+	JMenuBar menuBar = new JMenuBar();
+}
