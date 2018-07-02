3f2dacd1a29bbd0a8e9a846f3991fb59a13ffd47
Winston Lu <992090718@qq.com>
2018-07-03 01:23:07 +0800

    add

---
+++ demo1/src/chapter12/chapter12_2/JOptionPaneTest.java
@@ -0,0 +1,14 @@
+package chapter12.chapter12_2;
+
+import javax.swing.JFrame;
+
+public class JOptionPaneTest {
+
+	JFrame jf = new JFrame("≤‚ ‘JOptionPane");
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
