341737c2ae60407bebcc3bff132ab7fc280e0659
Winston Lu <992090718@qq.com>
2018-07-02 23:44:56 +0800

    update

--- demo1/src/chapter12/chapter12_2/ImageViewer.java
+++ demo1/src/chapter12/chapter12_2/ImageViewer.java
@@ -1,10 +1,15 @@
 package chapter12.chapter12_2;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenuBar;
 
 public class ImageViewer {
 	final int PREVIEW_SIZE = 100;
 	JFrame jf = new JFrame("¼òµ¥Í¼Æ¬²é¿´Æ÷");
 	JMenuBar menuBar = new JMenuBar();
+	JLabel label = new JLabel();
+	JFileChooser chooser = new JFileChooser(".");
+	JLabel accessory = new JLabel();
 }
