6c59f3fbf442cc17661086b676fb434dc8b6dfc2
Winston Lu <992090718@qq.com>
2018-07-06 10:54:29 +0800

    add

---
+++ demo1/src/chapter16Annotation/chapter16_2/c2/SuppressWarningsAnnotation.java
@@ -0,0 +1,22 @@
+package chapter16Annotation.chapter16_2.c2;
+@Deprecated
+class Demo3<T>{
+	private T var;
+	public T getVar() {
+		return var;
+	}
+	public void setVar(T var) {
+		this.var = var;
+	}
+}
+
+public class SuppressWarningsAnnotation {
+	@SuppressWarnings({"unchecked", "deprecation"})
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Demo3 d = new Demo3();
+		d.setVar("李兴华");
+		System.out.println("内容："+d.getVar());
+	}
+
+}
