ed4685b1417e9a5f5d5f8f790df337f89f4eeeca
Winston Lu <992090718@qq.com>
2018-07-16 15:32:56 +0800

    update

--- imooc_collection_map_demo/src/com/imooc/collection/MapTest.java
+++ imooc_collection_map_demo/src/com/imooc/collection/MapTest.java
@@ -2,6 +2,7 @@ package com.imooc.collection;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -46,12 +47,40 @@ public class MapTest {
 			}
 		}
 	}
+	
+	//删除Map中的数据
+	public void testRemove() {
+		Scanner console = new Scanner(System.in);
+		while(true) {
+			System.out.println("请输入要删除学生的ID");
+			String ID = console.next();
+			Student st = students.get(ID);
+			if(st == null) {
+				System.out.println("该ID不存在");
+				continue;
+			}
+			students.remove(ID);
+			System.out.println("成功删除学生:"+st.name);
+			break;
+		}
+	}
+	
+	//遍历所有键值对
+	public void testEntrySet() {
+		Set<Entry<String, Student>> entrySet = students.entrySet();
+		for(Entry<String, Student> entry:entrySet) {
+			System.out.println("取得键:"+entry.getKey());
+			System.out.println("对应的值为："+entry.getValue().name);
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MapTest mt = new MapTest();
 		mt.testPut();
 		mt.testKeySet();
+		mt.testRemove();
+		mt.testEntrySet();
 		
 	}
 
