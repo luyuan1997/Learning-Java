548e7c9c06f0a76798d2f6eec30a3f66ddc94896
Winston Lu <992090718@qq.com>
2018-07-17 18:49:32 +0800

    add

---
+++ imooc_interface_demo/src/interfaces/TestOneInterface.java
@@ -0,0 +1,20 @@
+package interfaces;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+
+public class TestOneInterface extends UnitTestBase{
+	public TestOneInterface() {
+		super("classpath*:spring-ioc.xml");
+	}
+	
+	@Test
+	public void testHello() {
+		OneInterface oneInterface = super.getBean("oneInterface");
+		System.out.println(oneInterface.hello("我输入的参数："));
+	}
+
+
+}
---
+++ imooc_interface_demo/src/interfaces/UnitTestBase.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+public class UnitTestBase {
+
+	public UnitTestBase(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public OneInterface getBean(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
