d0ab51e9247704c2c93e7dc7116b27ffb74543dd
Winston Lu <992090718@qq.com>
2018-07-26 18:30:23 +0800

    update

--- Spring_day3_jdbcTemplate/src/cn/itcast/jdbc/JdbcTemplateDemo2.java
+++ Spring_day3_jdbcTemplate/src/cn/itcast/jdbc/JdbcTemplateDemo2.java
@@ -8,11 +8,37 @@ import java.sql.SQLException;
 
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 
 
 public class JdbcTemplateDemo2 {
+	
+	
+	//查询返回对象
+		@Test
+		public void testObject() {
+			//设置数据库信息
+			DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		//	dataSource.setDriverClassName("com.mysql.jbdc.Driver");
+			dataSource.setUrl("jdbc:mysql:///spring_day3");
+			dataSource.setUsername("root");
+			dataSource.setPassword("lu910208");
+					
+			//创建jdbcTemplate对象，设置数据源
+			JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+			//调用方法查询记录数
+			String sql = "select * from user where username=?";
+			//第二个参数需要自己写实现接口，自己做数据封装
+			User user = jdbcTemplate.queryForObject(sql, new MyRowMapper(), "lucy");
+			System.out.println(user);
+			
+		}
+	
+	
+	
+	
 	//jdbc实现代码
 	@Test
 	public void testJDBC(){
@@ -79,5 +105,21 @@ public class JdbcTemplateDemo2 {
 		System.out.println(count);
 	}
 		
+}
+
+class MyRowMapper implements RowMapper<User>{
+
+	@Override
+	public User mapRow(ResultSet rs, int num) throws SQLException {
+		// 从结果集里面把数据得到
+		String username = rs.getString("username");
+		String password = rs.getString("password");
+		//把得到的数据封装到对象里面
+		User user = new User();
+		user.setPassword(username);
+		user.setPassword(password);
+		return user;
+	}
+	
 }
 
