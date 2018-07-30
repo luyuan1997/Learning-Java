785dceeb2c5f9013264a48c81722885bb30e57ab
Winston Lu <992090718@qq.com>
2018-07-30 19:45:42 +0800

    add

---
+++ leetcode/src/easy/People.java
@@ -0,0 +1,14 @@
+package easy;
+
+public class People {
+	
+		private double height;
+
+		public double getHeight() {
+			return height;
+		}
+
+		public void setHeight(double height) {
+			this.height = height;
+		}
+}
