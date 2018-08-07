ecd10e92431c45819d434bec6eeb79f1641bf928
Winston Lu <992090718@qq.com>
2018-08-08 00:21:38 +0800

    add

--- girl/src/main/java/com/imooc/girl/HelloController.java
+++ girl/src/main/java/com/imooc/girl/HelloController.java
@@ -1,13 +1,26 @@
 package com.imooc.girl;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${cupSize}")
+    private String cupSize;
+
+    @Value("${age}")
+    private Integer age;
+
+    @Value("${content}")
+    private String content;
+
     @RequestMapping(value="/hello", method = RequestMethod.GET)
     public String say(){
-        return "Hello Spring Boot!";
+
+        //return "Hello Spring Boot!";
+        return content;
     }
 }
