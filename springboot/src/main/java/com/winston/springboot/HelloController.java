a134131e6c03c0502433cd5eb7e8456cc4bdfea8
Winston Lu <992090718@qq.com>
2018-08-12 23:11:12 +0800

    add

---
+++ springboot/src/main/java/com/winston/springboot/HelloController.java
@@ -0,0 +1,15 @@
+package com.winston.springboot;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello", method =  RequestMethod.GET)
+    public String say(){
+        return "Hello Spring Boot!";
+    }
+}
