21f1badcb9bb0b3e166912a68a57319811bbfd80
Winston Lu <992090718@qq.com>
2018-08-13 22:41:16 +0800

    add

---
+++ MyBatis/test/test/UserTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import pojo.User;
+
+public class UserTest {
+
+	@Test
+	public void testFindUserById() throws Exception{
+		String resource = "SqlMapConfig.xml";
+		//通过流将核心配置文件读取进来
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		//通过核心配置文件输入流来创建会话工厂
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+		//通过工厂创建会话
+		SqlSession openSession = factory.openSession();
+		//第一个参数：所调用的SQL语句(User.xml文件中的namespace属性和select语句中的id属性值)namespace+.+sql的id值
+		User user = openSession.selectOne("test.findUserById", 1);
+		System.out.println(user);
+		openSession.close();
+	}
+	
+	@Test
+	public void testFindUserByUserName() throws Exception{
+		String resource  = "SqlMapConfig.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+		SqlSession openSession = factory.openSession();
+		
+		List<User> list = openSession.selectList("test.findUserByUserName", "%王%");
+		System.out.println(list);
+		openSession.close();
+	}
+}
