acca2a0d9f2b59467be465f9f7154b6ae624e6db
Winston Lu <992090718@qq.com>
2018-08-16 21:46:20 +0800

    add

---
+++ o2o/src/main/java/com/imooc/o2o/entity/Shop.java
@@ -0,0 +1,110 @@
+package com.imooc.o2o.entity;
+
+import java.util.Date;
+
+public class Shop {
+
+	private Long shopId;
+	private String shopName;
+	private String shopDesc;
+	private String shopAddr;
+	private String phone;
+	private String shopImg;
+	private Integer priority;
+	private Date createTime;
+	private Date lastEditTime;
+	//-1不可用(客户无法再前端看到该店铺，并且店家无法登陆到店铺管理系统）
+	//0审核中：客户无法从前端看到展示，但是店家可以登陆到店铺系统进行商家产品以及对店铺进行管理
+	//1:可用，客户可以看到展示页，店家可以登陆到管理系统
+	private Integer enableStatus;
+	//超级管理员给店家的建议提醒
+	private String advice;
+	private Area area;
+	private PersonInfo personInfo;
+	private ShopCategory shopCategory;
+	public Long getShopId() {
+		return shopId;
+	}
+	public void setShopId(Long shopId) {
+		this.shopId = shopId;
+	}
+	public String getShopName() {
+		return shopName;
+	}
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+	public String getShopDesc() {
+		return shopDesc;
+	}
+	public void setShopDesc(String shopDesc) {
+		this.shopDesc = shopDesc;
+	}
+	public String getShopAddr() {
+		return shopAddr;
+	}
+	public void setShopAddr(String shopAddr) {
+		this.shopAddr = shopAddr;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getShopImg() {
+		return shopImg;
+	}
+	public void setShopImg(String shopImg) {
+		this.shopImg = shopImg;
+	}
+	public Integer getPriority() {
+		return priority;
+	}
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public Date getLastEditTime() {
+		return lastEditTime;
+	}
+	public void setLastEditTime(Date lastEditTime) {
+		this.lastEditTime = lastEditTime;
+	}
+	public Integer getEnableStatus() {
+		return enableStatus;
+	}
+	public void setEnableStatus(Integer enableStatus) {
+		this.enableStatus = enableStatus;
+	}
+	public String getAdvice() {
+		return advice;
+	}
+	public void setAdvice(String advice) {
+		this.advice = advice;
+	}
+	public Area getArea() {
+		return area;
+	}
+	public void setArea(Area area) {
+		this.area = area;
+	}
+	public PersonInfo getPersonInfo() {
+		return personInfo;
+	}
+	public void setPersonInfo(PersonInfo personInfo) {
+		this.personInfo = personInfo;
+	}
+	public ShopCategory getShopCategory() {
+		return shopCategory;
+	}
+	public void setShopCategory(ShopCategory shopCategory) {
+		this.shopCategory = shopCategory;
+	}
+	
+}
---
+++ o2o/src/main/java/com/imooc/o2o/entity/ShopCategory.java
@@ -0,0 +1,64 @@
+package com.imooc.o2o.entity;
+
+import java.util.Date;
+
+public class ShopCategory {
+
+	private Long shopCategoryId;
+	private String shopCategoryName;
+	private String shopCategoryDesc;
+	private String shopCategoryImg;
+	private Integer priority;
+	private Date creatTime;
+	private Date lastEditTime;
+	private ShopCategory parent;
+	public Long getShopCategoryId() {
+		return shopCategoryId;
+	}
+	public void setShopCategoryId(Long shopCategoryId) {
+		this.shopCategoryId = shopCategoryId;
+	}
+	public String getShopCategoryName() {
+		return shopCategoryName;
+	}
+	public void setShopCategoryName(String shopCategoryName) {
+		this.shopCategoryName = shopCategoryName;
+	}
+	public String getShopCategoryDesc() {
+		return shopCategoryDesc;
+	}
+	public void setShopCategoryDesc(String shopCategoryDesc) {
+		this.shopCategoryDesc = shopCategoryDesc;
+	}
+	public String getShopCategoryImg() {
+		return shopCategoryImg;
+	}
+	public void setShopCategoryImg(String shopCategoryImg) {
+		this.shopCategoryImg = shopCategoryImg;
+	}
+	public Integer getPriority() {
+		return priority;
+	}
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+	public Date getCreatTime() {
+		return creatTime;
+	}
+	public void setCreatTime(Date creatTime) {
+		this.creatTime = creatTime;
+	}
+	public Date getLastEditTime() {
+		return lastEditTime;
+	}
+	public void setLastEditTime(Date lastEditTime) {
+		this.lastEditTime = lastEditTime;
+	}
+	public ShopCategory getParent() {
+		return parent;
+	}
+	public void setParent(ShopCategory parent) {
+		this.parent = parent;
+	}
+	
+}
