43e12492cca4843fb5a653100a50e92f7c890e08
Winston Lu <992090718@qq.com>
2018-08-29 16:09:06 +0800

    add

---
+++ login/src/com/itcast/dao/UserDao.java
@@ -0,0 +1,30 @@
+package com.itcast.dao;
+
+import java.sql.SQLException;
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+
+import com.itcast.domain.User;
+import com.itcast.utils.DataSourceUtils;
+
+public class UserDao {
+	/**
+	 * 登录 
+	 * @param username
+	 * @param password
+	 * @return
+	 * @throws SQLException 
+	 */
+	public User getUserByUsernameAndPwd(String username, String password) throws SQLException {
+		//创建queryrunner
+		QueryRunner qr = new QueryRunner(DataSourceUtils.getDataSource());
+		
+		//编写sql
+		String sql="select * from user where username=? and password=?";
+		//执行sql
+		User user = qr.query(sql, new BeanHandler<>(User.class),username,password);
+		return user;
+	}
+
+}
---
+++ login/src/com/itcast/utils/DataSourceUtils.java
@@ -0,0 +1,95 @@
+package com.itcast.utils;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class DataSourceUtils {
+	private static ComboPooledDataSource ds = new ComboPooledDataSource();
+
+	/**
+	 * 获取数据源
+	 */
+	public static DataSource getDataSource() {
+		return ds;
+	}
+	/**
+	 * 获取连接
+	 * @throws SQLException 
+	 */
+	public static Connection getConnection() throws SQLException {
+		return ds.getConnection();
+	}
+	/**
+	 * 释放资源
+	 * 
+	 * @param conn
+	 *            链接
+	 * @param st
+	 *            语句执行这
+	 * @param rs
+	 *            结果集
+	 */
+	public static void closeResource(Connection conn, Statement st, ResultSet rs) {
+
+		closeResultSet(rs);
+		closeStatement(st);
+		closeConn(conn);
+	}
+
+	/**
+	 * 释放链接
+	 * 
+	 * @param conn
+	 *            链接
+	 */
+	public static void closeConn(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			conn = null;// 利于java垃圾回收机制快速收走
+		}
+	}
+
+	/**
+	 * 释放语句执行者
+	 * 
+	 * @param st
+	 *            语句执行者
+	 */
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			st = null;// 利于java垃圾回收机制快速收走
+		}
+	}
+
+	/**
+	 * 释放结果集
+	 * 
+	 * @param rs
+	 *            结果集
+	 */
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			rs = null;// 利于java垃圾回收机制快速收走
+		}
+	}
+}
