67a6638c59d5edcd0ed4120ee327b78d42e747bd
lu <992090718@qq.com>
2019-01-24 17:19:03 +0800

    add and delete

---
+++ p20190124/CountdownLatchTest2.java
@@ -0,0 +1,9 @@
+package p20190124;
+
+public class CountdownLatchTest2 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- p20190124/TestCountDownLatch.java
+++
@@ -1,7 +0,0 @@
-package p20190124;
-
-import java.util.logging.Logger;
-
-public class TestCountDownLatch {
-	private static Logger LOGGER = Logger.getLogger("log", TestCountDownLatch.class);
-}
