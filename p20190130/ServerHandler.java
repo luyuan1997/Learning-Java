ef89c7e3af032fb8d2013192a2799981c3b7d7b0
lu <992090718@qq.com>
2019-01-30 22:32:30 +0800

    add

--- p20190130/ServerHandler.java
+++ p20190130/ServerHandler.java
@@ -1,7 +1,25 @@
 package p20190130;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
 
 public class ServerHandler extends ChannelHandlerAdapter {
-
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		ByteBuf buf = (ByteBuf) msg;
+		byte[] data = new byte[buf.readableBytes()];
+		buf.readBytes(data);
+		String request = new String(data, "utf-8");
+		System.out.println("Server: " + request);
+		//写给客户端
+		String response = "我是反馈的信息";
+		ctx.writeAndFlush(Unpooled.copiedBuffer("888".getBytes()));
+		//.addListener(ChannelFutureListener.CLOSE);
+	}
+	
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		cause.printStackTrace();
+		ctx.close();
+	}
 }
